1c5bd9e2101d810c9fa1ea7d9773191c144993d1
Morgan McDonald <morganmcdee13@outlook.com>
2021-02-11 21:05:49 +0000

    reviews now implemented in carousel

--- reviews/models.py
+++ reviews/models.py
@@ -6,11 +6,13 @@ class Review(models.Model):
     media_title = models.CharField(max_length=200)
     media_id = models.IntegerField()
     media_type = models.CharField(max_length=100, blank=True)
+    media_poster = models.CharField(max_length=200, blank=True)
     username = models.CharField(max_length=200)
     review_title = models.CharField(max_length=200)
     review_content = models.TextField()
     rating = models.DecimalField(max_digits=2, decimal_places=1, default=0)
     review_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
+    user_image_url = models.CharField(max_length=300, blank=True)
     def __str__(self):
         return self.review_title
--- reviews/views.py
+++ reviews/views.py
@@ -8,13 +8,15 @@ def review(request):
         media_title = request.POST['media_title']
         media_id = request.POST['media_id']
         media_type = request.POST['media_type']
+        media_poster = request.POST['media_poster']
         username = request.POST['username']
         review_title = request.POST['review_title']
         review_content = request.POST['review_content']
         rating = request.POST['rating']
         user_id = request.POST['user_id']
+        user_image_url = request.POST['user_image_url']
 
-        review = Review(media_title=media_title, media_id=media_id, media_type=media_type, username=username, review_title=review_title, review_content=review_content, rating=rating, user_id=user_id)
+        review = Review(media_title=media_title, media_id=media_id, media_type=media_type, media_poster=media_poster, username=username, review_title=review_title, review_content=review_content, rating=rating, user_id=user_id, user_image_url=user_image_url)
         review.save()
 
         messages.success(request, 'Your review was added successfully')
