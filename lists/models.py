193d9fab42ce2785de1324331b203c1e344c1132
Morgan McDonald <morganmcdee13@outlook.com>
2021-02-17 13:17:23 +0000

    Watchlist created for user on profile creation

--- lists/models.py
+++ lists/models.py
@@ -5,9 +5,9 @@ class List(models.Model):
     list_name = models.CharField(max_length=200)
     owner_id = models.IntegerField()
     owner_username = models.CharField(max_length=200)
-    title_id = models.IntegerField()
-    title_name = models.CharField(max_length=200)
-    title_type = models.CharField(max_length=200)
-    title_poster = models.CharField(max_length=200)
+    title_id = models.IntegerField(blank=True)
+    title_name = models.CharField(max_length=200, blank=True)
+    title_type = models.CharField(max_length=200, blank=True)
+    title_poster = models.CharField(max_length=200, blank=True)
     def __str__(self):
         return self.list_name
--- lists/signals.py
+++ lists/signals.py
@@ -1,8 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
-from lists.models import List
+from .models import List
 
 
 @receiver(post_save, sender=User)
@@ -14,8 +13,3 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-
-@receiver(post_save, sender=User)
-def create_list(sender, instance, created, **kwargs):
-    if created:
-        List.objects.create(list_name='Watchlist', owner_id=instance.id, owner_username=instance.username, title_id=0, title_name='', title_type='', title_poster='')
