2af0e5bc0c5adb88e874a49b55bda62bbae0850c
Morgan McDonald <morganmcdee13@outlook.com>
2021-02-19 16:49:47 +0000

    adding and removing things to lists now working as it should
    fixing add to list has now made everything appear on the page when it shouldn't

--- lists/urls.py
+++ lists/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list', views.list, name='list')
+    path('list', views.list, name='list'),
+    path('delete_list', views.delete_list, name='delete_list'),
+    path('delete_list_item', views.delete_list_item, name='delete_list_item')
 ]
--- lists/views.py
+++ lists/views.py
@@ -18,3 +18,28 @@ def list(request):
 
         messages.success(request, 'Added to list successfully')
         return redirect('result')
+
+def delete_list(request):
+    if request.method == 'POST':
+        list_name = request.POST['list_name']
+        owner_id = request.POST['owner_id']
+
+        item = List.objects.filter(list_name=list_name, owner_id=owner_id)
+        print(item)
+        List.objects.filter(list_name=list_name).delete()
+        messages.success(request, 'Your list was deleted successfully.')
+        return redirect('account')
+
+def delete_list_item(request):
+    if request.method == 'POST':
+        list_name = request.POST['list_name']
+        owner_id = request.POST['owner_id']
+        title_id = request.POST['title_id']
+        title_name = request.POST['title_name']
+        title_type = request.POST['title_type']
+
+        item = List.objects.get(list_name=list_name, owner_id=owner_id, title_id=title_id, title_name=title_name, title_type=title_type)
+        print(item)
+        List.objects.filter(list_name=list_name, title_id=title_id, title_name=title_name, title_type=title_type).delete()
+        messages.success(request, '%s was successfully removed from your list.' % title_name)
+        return redirect('account')
