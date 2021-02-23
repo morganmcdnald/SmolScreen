47af9695d92c300a5edab145e822ac11fd41262a
Morgan McDonald <morganmcdee13@outlook.com>
2021-02-23 16:18:26 +0000

    beginning of restyle for results

--- SmolScreen/static/js/main.js
+++ SmolScreen/static/js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip();
-    getMovie();
+    getResult();
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
         movieSearched(searchText);
         e.preventDefault();
     });
+    if (movieType == 'person') {
+        document.getElementById('reviews').innerHTML = "";
+        document.getElementById('likeBtn').innerHTML = "";
+        document.getElementById('listModal').innerHTML = "";
+    }
+    if ((movieType == 'movie') || (movieType == 'tv')) {
+        document.getElementById('ActorLikeBtn').innerHTML = "";
+    }
 });
 
 
@@ -18,7 +26,7 @@ setTimeout(function() {
 
 function getMovies() {
     let searchText = localStorage.getItem('searchEntry');
-    axios.get('https://api.themoviedb.org/3/search/multi?api_key=994d34ea949ad278ddad346696fea280&language=en-US&query='+searchText+'&page=1&include_adult=false').then((response) => {
+    axios.get('https://api.themoviedb.org/3/search/multi?api_key=994d34ea949ad278ddad346696fea280&language=en-US&query='+searchText+'&page=1&include_adult=false&append_to_response=external_ids').then((response) => {
         console.log(response);
         let movies = response.data.results;
         let output = '';
@@ -106,7 +114,7 @@ function getMovies() {
     });
 }
 
-function movieSelected(id, media_type, media_title, media_poster) {
+function movieSelected(id, media_type, media_title, media_poster, imdb) {
     sessionStorage.setItem('movieId', id);
     sessionStorage.setItem('type', media_type);
     sessionStorage.setItem('title', media_title);
@@ -569,3 +577,308 @@ function getHotTV() {
         console.log(err);
     });
 }
+
+function getResult() {
+    let movieId = localStorage.getItem('movieId');
+    let movieType = localStorage.getItem('type');
+    let lastSearched = localStorage.getItem('searchEntry');
+    if (movieType == 'movie') {
+        axios.get('https://api.themoviedb.org/3/movie/'+movieId+'?api_key=994d34ea949ad278ddad346696fea280&language=en-US&append_to_response=credits').then((response) => {
+        console.log(response);
+        let movie = response.data;
+        let genrePath = response.data.genres;
+        let directors = response.data.credits.crew;
+        let genreList = "";
+        let directorList = "";
+        let actorPath =response.data.credits.cast;
+        let actors = [];
+        let genres = [];
+        let output = "";
+        let imdb = movie.imdb_id;
+        sessionStorage.setItem('imdb', imdb);
+        localStorage.setItem('imdb', imdb);
+        $.each(genrePath, (index, genre) => {
+            genres.push(" " + genre.name);
+        });
+        $.each(directors, (index, director) => {
+            if (director.job == 'Director') {
+                directorList += `${director.name}`
+            }
+        });
+        $.each(actorPath, (index, actor) => {
+            actors.push(" " + actor.name);
+        });
+        if (lastSearched == "") {
+            left_output = `<a href="/" class="btn btn-secondary">Back to Home</a>`;
+        }
+        else {
+            left_output = `<a onclick="movieSearched('${lastSearched}')" class="btn btn-secondary">Back to Search</a>`;
+        }
+        imdb_output = `<a href="https://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary text-white">View on IMDB</a>`;
+        output += `
+            <div class="row">
+                <div class="col-md-4">
+                    <br>
+                    <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <br>
+                    <h2>${movie.title}</h2>
+                    <h5 class="tagline">${movie.tagline}</h5>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${genres}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+                        <li class="list-group-item"><strong>Rating:</strong> ${movie.vote_average}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${directorList}</li>
+                        <li class="list-group-item"><strong>Plot:</strong> ${movie.overview}</li>
+                    </ul>
+                </div>
+            </div>
+            <br>
+            <br>
+            <h3 class="text-center mb-3">Top Billed Cast</h3>
+            <div class="row">
+        `;
+        $.each(actorPath.slice(0,8), (index, actor) => {
+            if (actor.profile_path != null) {
+                output += `
+                <div class="col-md-3">
+                    <div class="cast well text-center">
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', '${actor.profile_path}')"><img src="https://image.tmdb.org/t/p/w342${actor.profile_path}" alt="Profile Pic"></a>
+                        <br>
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', '${actor.profile_path}')"><h4>${actor.name}</h4></a>
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', '${actor.profile_path}')"><h6>${actor.character}</h6></a>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            `;
+            }
+            if (actor.profile_path == null) {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="https://i.imgur.com/5E6Su16.png" alt="Default Profile Pic">
+                        <br>
+                        <h4>${actor.name}</h4>
+                        <h6>${actor.character}</h6>
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', 'https://i.imgur.com/5E6Su16.png')" class="btn btn-primary" href="#">View Profile</a>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            `;
+            }
+        });
+
+        output += `</div> `;
+
+        $('#movie').html(output);
+        $('#left').html(left_output);
+        $('#imdb').html(imdb_output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    if (movieType == 'tv') {
+        axios.get('https://api.themoviedb.org/3/tv/'+movieId+'?api_key=994d34ea949ad278ddad346696fea280&language=en-US&append_to_response=credits,external_ids').then((response) => {
+        console.log(response);
+        let movie = response.data;
+        let genrePath = response.data.genres;
+        let actorPath =response.data.credits.cast;
+        let actors = [];
+        let genres = [];
+        let output = "";
+        $.each(genrePath, (index, genre) => {
+            genres.push(" " + genre.name);
+        });
+        $.each(actorPath, (index, actor) => {
+            actors.push(" " + actor.name);
+        });
+        if (lastSearched == "") {
+            left_output = `<a href="/" class="btn btn-secondary">Back to Home</a>`;
+        }
+        else {
+            left_output = `<a onclick="movieSearched('${lastSearched}')" class="btn btn-secondary">Back to Search</a>`;
+        }
+        imdb_output = `<a href="https://imdb.com/title/${movie.external_ids.imdb_id}" target="_blank" class="btn btn-primary text-white">View on IMDB</a>`;
+        output += `
+            <div class="row">
+                <div class="col-md-4">
+                    <br>
+                    <img src="https://image.tmdb.org/t/p/w342${movie.poster_path}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <br>
+                    <h2>${movie.name}</h2>
+                    <h5>${movie.tagline}</h5>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${genres}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.first_air_date}</li>
+                        <li class="list-group-item"><strong>Rating:</strong> ${movie.vote_average}</li>
+                        <li class="list-group-item"><strong>Status:</strong> ${movie.status}</li>
+                        <li class="list-group-item"><strong>Seasons:</strong> ${movie.number_of_seasons}</li>
+                        <li class="list-group-item"><strong>Episodes:</strong> ${movie.number_of_episodes}</li>
+                        <li class="list-group-item"><strong>Plot:</strong> ${movie.overview}</li>
+                    </ul>
+                </div>
+            </div>
+            <br>
+            <br>
+            <h3 class="text-center mb-3">Top Billed Cast</h3>
+            <div class="row">
+        `;
+        $.each(actorPath.slice(0,8), (index, actor) => {
+            if (actor.profile_path != null) {
+                output += `
+                <div class="col-md-3">
+                    <div class="cast well text-center">
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', '${actor.profile_path}')"><img src="https://image.tmdb.org/t/p/w342${actor.profile_path}" alt="Profile Pic"></a>
+                        <br>
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', '${actor.profile_path}')"><h4>${actor.name}</h4></a>
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', '${actor.profile_path}')"><h6>${actor.character}</h6></a>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            `;
+            }
+            if (actor.profile_path == null) {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="https://i.imgur.com/5E6Su16.png" alt="Default Profile Pic">
+                        <br>
+                        <h4>${actor.name}</h4>
+                        <h6>${actor.character}</h6>
+                        <a onclick="movieSelected('${actor.id}', 'person', '${actor.name}', 'https://i.imgur.com/5E6Su16.png')" class="btn btn-primary" href="#">View Profile</a>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            `;
+            }
+        });
+
+        output += `</div>`;
+
+        $('#movie').html(output);
+        $('#left').html(left_output);
+        $('#imdb').html(imdb_output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    if (movieType == 'person') {
+        axios.get('https://api.themoviedb.org/3/person/'+movieId+'?api_key=994d34ea949ad278ddad346696fea280&language=en-US&append_to_response=combined_credits,external_ids').then((response) => {
+        console.log(response);
+        let movie = response.data;
+        let creditsPath = response.data.combined_credits.cast;
+        let credits = [];
+        let output = "";
+        $.each(creditsPath, (index, credit) => {
+            credits.push(credit);
+        });
+        var sortedCredits = credits.sort((a, b) => b.vote_count - a.vote_count);
+        console.log(sortedCredits);
+        if (lastSearched == "") {
+            left_output = `<a href="/" class="btn btn-secondary">Back to Home</a>`;
+        }
+        else {
+            left_output = `<a onclick="movieSearched('${lastSearched}')" class="btn btn-secondary">Back to Search</a>`;
+        }
+        imdb_output = `<a href="https://imdb.com/name/${movie.imdb_id}" target="_blank" class="btn btn-primary text-white">View on IMDB</a>`;
+        if (movie.profile_path == null) {
+            output += `
+            <div class="row">
+                <div class="col-md-4">
+                    <br>
+                    <img src="https://i.imgur.com/5E6Su16.png" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <br>
+                    <h2>${movie.name}</h2>
+                    <br>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Birthday:</strong> ${movie.birthday}</li>
+                        <li class="list-group-item"><strong>Birth Place:</strong> ${movie.place_of_birth}</li>
+                        <li class="list-group-item"><strong>Biography:</strong> ${movie.biography}</li>
+                    </ul>
+                </div>
+            </div>
+            <br>
+            <br>
+            <h3 class="text-center mb-3">Top Rated Credits</h3>
+            <div class="row">
+        `;
+        }
+        else if (movie.profile_path != null) {
+            output += `
+            <div class="row">
+                <div class="col-md-4">
+                    <br>
+                    <img src="https://image.tmdb.org/t/p/w342${movie.profile_path}" class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <br>
+                    <h2>${movie.name}</h2>
+                    <br>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Birthday:</strong> ${movie.birthday}</li>
+                        <li class="list-group-item"><strong>Birth Place:</strong> ${movie.place_of_birth}</li>
+                        <li class="list-group-item"><strong>Biography:</strong> ${movie.biography}</li>
+                    </ul>
+                </div>
+            </div>
+            <br>
+            <br>
+            <h3 class="text-center mb-3">Top Rated Credits</h3>
+            <div class="row">
+        `;
+        }
+
+        $.each(sortedCredits.slice(0,8), (index, credit) => {
+            if ((credit.media_type == 'movie') && (credit.poster_path != null)) {
+            output += `
+                <div class="col-md-3">
+                    <div class="cast well text-center">
+                        <a onclick="movieSelected('${credit.id}', '${credit.media_type}', '${credit.title}', '${credit.poster_path}')"><img src="https://image.tmdb.org/t/p/w342${credit.poster_path}"></a>
+                        <br>
+                        <a onclick="movieSelected('${credit.id}', '${credit.media_type}', '${credit.title}', '${credit.poster_path}')"><h4>${credit.title}</h4></a>
+                        <a onclick="movieSelected('${credit.id}', '${credit.media_type}', '${credit.title}', '${credit.poster_path}')"><h6>${credit.character}</h6></a>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            `;
+            }
+            if ((credit.media_type == 'tv') && (credit.poster_path != null)) {
+                output += `
+                    <div class="col-md-3">
+                        <div class="cast well text-center">
+                        <a onclick="movieSelected('${credit.id}', '${credit.media_type}', '${credit.title}', '${credit.poster_path}')"><img src="https://image.tmdb.org/t/p/w342${credit.poster_path}"></a>
+                            <br>
+                            <a onclick="movieSelected('${credit.id}', '${credit.media_type}', '${credit.title}', '${credit.poster_path}')"><h4>${credit.name}</h4></a>
+                            <a onclick="movieSelected('${credit.id}', '${credit.media_type}', '${credit.title}', '${credit.poster_path}')"><h6>${credit.character}</h6></a>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                `;
+                }
+        });
+
+        output += `</div> `;
+        $('#movie').html(output);
+        $('#left').html(left_output);
+        $('#imdb').html(imdb_output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+  }
